e9a34d9f6406801820bda2a70edf7f9d13dde260
Natasha Chernis <natashachernis@gmail.com>
2019-03-10 21:52:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,8 @@ require "pry"
 
 def get_first_name_of_season_winner(data, season_arg)
   binding.pry
-  data[season_arg]
+  data[season_arg].find {|contestant| 
+    contestant["name"].split(" ").first if contestant["status"] == "Winner"}
 end
 
 def get_contestant_name(data, occupation)
